dd6e5d4bcfdf042afb42cad14f0843605e68bde0
Scott <saxlbeckett@gmail.com>
2021-03-31 13:30:41 -0400

    Figured out what was wrong with the cell formatting

--- react-tetris/src/components/styles/StyledStage.js
+++ react-tetris/src/components/styles/StyledStage.js
@@ -6,7 +6,7 @@ export const StyledStage = styled.div`
     ${props => props.height},
     calc(25vw / ${props => props.width})
   );
-  grid-template-columns: repeat(${props => props.width}, 1fr);
+  grid-template-columns: repeat(${props => props.width}, 2rem);
   grid-gap: 1px;
   border: 2px;
   background: #111;
